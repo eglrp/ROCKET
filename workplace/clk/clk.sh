c6131ec69beee1bf76f6472e3c06f3417f121763
shjzhang <shjzhang@sgg.whu.edu.cn>
2016-08-17 10:11:09 +0800

    batch processing shell script for clock estimation is finished

--- workplace/clk/clk.sh
+++ workplace/clk/clk.sh
@@ -4,7 +4,7 @@
 # get the rinex file list
 ##############################
 > clk.rnxlist
-get_rnx.sh -b "2011 10 1 0 0 0" -e "2011 10 2 0 0 0" -i 24 -m  -a "IGS" -u "../../tables/url.list" -s "clk.60.reflist" -l "clk" -p "/Users/shjzhang/Documents/data/IGS/data" > get_rnx.log 
+get_rnx.sh -b "2011 10 1 0 0 0" -e "2011 10 4 0 0 0" -i 24 -m  -a "IGS" -u "../../tables/url.list" -s "clk.60.reflist" -l "clk" -p "$HOME/data/IGS/data" > get_rnx.log 
 
 ##############################
 # get the ephemeris file list
@@ -13,7 +13,7 @@ get_rnx.sh -b "2011 10 1 0 0 0" -e "2011 10 2 0 0 0" -i 24 -m  -a "IGS" -u "../.
 > clk.clklist
 > clk.erplist
 > clk.ssclist
-get_eph.sh -b "2011 09 30 0 0 0" -e "2011 10 2 0 0 0" -i 24 -u "../../tables/url.list" -t "type.list" -l "clk" -p "/Users/shjzhang/Documents/data/IGS/data" > get_eph.log 
+get_eph.sh -b "2011 10 1 0 0 0" -e "2011 10 4 0 0 0" -i 24 -u "../../tables/url.list" -t "type.list" -l "clk" -p "$HOME/data/IGS/data" > get_eph.log 
 
 ##############################
 # get the msc file list
@@ -25,8 +25,8 @@ do
 done
 
 > msc.txt
-# now, let's merge all the msc files together for pppar positioning
-ls /Users/shjzhang/Documents/data/IGS/data/*.msc | while read line
+# now, let's merge all the msc files together for clk estimation 
+ls $HOME/data/IGS/data/*.msc | while read line
 do
   echo $line;
   cat $line >> msc.txt
@@ -34,4 +34,5 @@ done
 
 # now, Let's compute the satellite clock products
 # warning, please pay attention to the option CASE.
-clk -m msc.txt -r clk.rnxlist -s clk.ephlist -e clk.erplist -D clk.dcb_p1c1list -O whu2740_2790.clk
+clk -m msc.txt -r clk.rnxlist -s clk.ephlist -e clk.erplist -D clk.dcb_p1c1list -O whu2740_2760.clk
+
