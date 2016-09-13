dfa50c627add4b95d9e2e4851ec794cd3a496206
LeiShramana <lezhao@whu.edu.cn>
2016-09-13 10:51:55 +0800

    modify upd

---
+++ tests/updTest/newWayExe/work.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Download files needed.
+ln -f -s /Users/zhaolei/ROCKET/tables/OCEAN-GOT00.dat OCEAN-GOT00.dat
+ln -f -s /Users/zhaolei/ROCKET/tables/URL_LIST.txt url.list
+ln -f -s /Users/zhaolei/ROCKET/tables/PRN_GPS PRN_GPS
+ln -f -s /Users/zhaolei/ROCKET/tables/igs08.atx igs08.atx
+ln -f -s /Users/zhaolei/ROCKET/tables/ppp_type.list ppp_type.list
+
+proj="updTest2"
+#get_rnx.sh -b "2011 10 9 0 0 0" -e "2011 10 10 0 0 0" -i 24 -a "IGS" -u "url.list" -s "$proj.stalist.test" -l "$proj" -p "./rinex/" 
+#get_rnx.sh -b "2011 10 9 0 0 0" -e "2011 10 10 0 0 0" -i 24 -a "IGS" -u "url.list" -s "tmp.sta" -l "$proj" -p "./rinex/" 
+
+#get_eph.sh -b "2011 10 9 0 0 0" -e "2011 10 10 0 0 0" -i 24 -a IGS -u "url.list" -t "ppp_type.list" -l "$proj" -p "./Products/"
+
+# convert ssc2msc
+#cat $proj.ssclist | while read line
+#do
+#	ssc2msc -s $line
+#done
+#
+## merge all *.msc files
+#ls *.msc | while read line
+#do
+#	echo $line
+#	cat $line >> msc.txt
+#done
+
+upd -m msc.txt -r $proj.rnxlist -s $proj.ephlist -e $proj.erplist	-O whu2820.50.upd.new 
+
+
+
+
